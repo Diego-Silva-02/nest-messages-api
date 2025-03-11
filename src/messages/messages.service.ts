f115e2e749502b81a25268386894f484b444ecae
Diego-Silva-02 <diegosilva22a@gmail.com>
2025-03-11 14:55:44 -0300

    feat: added exceptions in messagesService

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Message } from './entities/message.entity';
 
 @Injectable()
@@ -15,43 +15,65 @@ export class MessagesService {
         }
     ];
 
+    throwNotFoundError() {
+        // throw new HttpException('Message not found.', HttpStatus.NOT_FOUND);
+        throw new NotFoundException('Message not found.');
+    }
+    
+
     findAll(): Message[] {
         return this.messages;
     }
 
     findOne(id: string): Message {
-        return this.messages.find(item => item.id === +id);
+        const message = this.messages.find(item => item.id === +id);
+
+        if (message)
+            return message;
+
+        this.throwNotFoundError();
     }
 
-    create(body: any) {
+    create(body: any): Message {
         this.lastId++;
         const id = this.lastId;
-        const newMessage = {
+        const newMessage: Message = {
             id,
             ...body
         }
 
         this.messages.push(newMessage);
+
+        return newMessage;
     }
 
-    update(id: string, body: any) {
+    update(id: string, body: any): Message {
         const messageIndex = this.messages.findIndex(item => item.id === +id);
 
-        if (messageIndex) {
-            const updatedMessage = this.messages[messageIndex];
-
-            this.messages[messageIndex] = {
-                ...updatedMessage,
-                ...body
-            }
+        if(messageIndex < 0) {
+            this.throwNotFoundError();
         }
+
+        const updatedMessage = this.messages[messageIndex];
+
+        this.messages[messageIndex] = {
+            ...updatedMessage,
+            ...body
+        }
+
+        return this.messages[messageIndex];
     }
 
     remove(id: string) {
         const messageIndex = this.messages.findIndex(item => item.id === +id);
 
-        if (messageIndex) {
-            this.messages.splice(messageIndex, 1);
+        if(messageIndex < 0) {
+            this.throwNotFoundError();
         }
+
+        const message = this.messages[messageIndex];
+        this.messages.splice(messageIndex, 1);
+
+        return message;
     }
 }
