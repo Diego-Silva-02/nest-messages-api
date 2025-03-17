9cd4d9168d4e3b346d4b48eca39f5f9e4a24d0f1
Diego-Silva-02 <diegosilva22a@gmail.com>
2025-03-17 08:48:00 -0300

    feat: added validations to the aplication

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // require to install 'class-validator' and 'class-transformer' lib's
+  // using this you can  validate object's in the request
+  app.useGlobalPipes(new ValidationPipe()); 
+  
   await app.listen(3000);
 }
 bootstrap();
--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,5 +1,24 @@
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
 export class UpdateMessageDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
+    @MaxLength(255)
+    @IsOptional()
     readonly text?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
+    @MaxLength(50)
+    @IsOptional()
     readonly from?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
+    @MaxLength(50)
+    @IsOptional()
     readonly to?: string;
 }
